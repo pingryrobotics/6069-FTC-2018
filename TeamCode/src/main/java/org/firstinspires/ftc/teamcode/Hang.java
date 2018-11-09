82c234fcb98b2afc8fb5858bd471e055c330ca99
mchan2021 <34148244+mchan2021@users.noreply.github.com>
2018-11-09 16:24:27 -0500

    Hang before Bayonne

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hang.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hang.java
@@ -3,13 +3,18 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 public class Hang {
     private DcMotor winch;
+    private Servo latchServo;
 
     public Hang(HardwareMap hardwareMap) {
         winch = hardwareMap.get(DcMotor.class, "winch");
+        winch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        winch.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        latchServo = hardwareMap.get(Servo.class, "latch servo");
     }
 
     public void hangUp(){
@@ -20,6 +25,12 @@ public class Hang {
     }
     public void stopHang(){
         winch.setPower(0);
+    }
+    public void unlatch(){
+        latchServo.setPosition(0.25);
+    }
+    public void latch(){
+        latchServo.setPosition(0.75);
     }
 
 
