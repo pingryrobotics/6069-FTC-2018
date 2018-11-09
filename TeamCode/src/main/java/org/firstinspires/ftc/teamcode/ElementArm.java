00cbc6449b323e377d98ca6cf48f4b9ebbe8c973
mchan2021 <34148244+mchan2021@users.noreply.github.com>
2018-11-09 16:21:46 -0500

    Commented out section is for the intake not built yet before Bayonne

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElementArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElementArm.java
@@ -5,45 +5,58 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class ElementArm {
-    private DcMotor armExtension;
+    private DcMotor armRotation;
     private DcMotor wrist;
     private Servo intake;
     private Servo collectionBox;
+
+    private final int outerLimit = 114;
     double pushSpeed = 0.75;
     double pullSpeed = 0.75;
     public static void main(String args[]){
 
     }
     public ElementArm(HardwareMap hardwareMap){
-        armExtension = hardwareMap.get(DcMotor.class, "armExtension");
+        armRotation = hardwareMap.get(DcMotor.class, "armExtension");
         wrist = hardwareMap.get(DcMotor.class, "wrist");
         intake = hardwareMap.get(Servo.class, "ziptieIntake");
         collectionBox = hardwareMap.get(Servo.class, "collectionBox");
+
+        armRotation.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //collectionBox.scaleRange(0,2.0/3);
+    }
+
+    public void resetEncoders(){
+        armRotation.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armRotation.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     //A note for these current values for the set positions as of 10.15.18 they are just filled in and will be adjusted later.
-    public void extend(double speed){
-        armExtension.setPower(speed);
+    public void rotateOut(double speed){
+        if(armRotation.getCurrentPosition() >= outerLimit){
+            armRotation.setPower(0);
+        }else {
+            armRotation.setPower(speed);
+        }
     }
-    public void retract(double speed){
-        armExtension.setPower(-speed);
+    public void rotateBack(double speed){
+        armRotation.setPower(-speed);
     }
-    public void rotateUp(){
-        wrist.setTargetPosition(1);
-    }
-    public void rotateDown(){
-        wrist.setTargetPosition(-1);
-    }
+
     public void outtake(){
-        collectionBox.setPosition(-0.5);
+        collectionBox.setPosition(.5);
     }
+    /*
     public void intake(){
         intake.setDirection(Servo.Direction.REVERSE);
     }
     public void stopExtend(){
-        armExtension.setPower(0);
+        armRotation.setPower(0);
     }
+
     public void stopSpin(){
         intake.setPosition(0);
     }
+*/
 }
